a6a7980c9e1c8b69c17e02a1edbce3ace4614688
hsibm <48910328+hsibm@users.noreply.github.com>
2019-04-11 16:33:51 +0200

    Create videosColumn.js

---
+++ app/src/components/videosColumn.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {connect} from 'react-redux'; 
+import {fetchVideos} from '../actions/videoActions';
+import TableRowVideo from './tablerowvideo';
+var listVideos
+class videos extends React.Component { 
+constructor() {
+super(); 
+}
+
+componentDidMount() { this.props.fetchVideos(); }
+
+render() { 
+   
+        listVideos = this.props.videos.map(video => 
+            <TableRowVideo key={video._id} video={video}/>
+
+);
+          
+        
+             
+
+ 
+return (
+     
+<div>
+     
+<center><h1>les dernières vidéos d'actualité </h1></center>
+ {listVideos}   
+</div>
+)
+
+}
+} 
+
+const mapStateToProps = (state, ownProps) => {
+return {
+clients : state.clients,
+videos: state.videos
+};
+}
+
+const mapDispatchToProps = (dispatch) => {
+return {
+    fetchVideos :()=> dispatch(fetchVideos())
+};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(videos);
+
+
