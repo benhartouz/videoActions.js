3de20b19491c03e00bf77aadb6f534db62916892
hsibm <48910328+hsibm@users.noreply.github.com>
2019-04-11 16:27:14 +0200

    Add files via upload

---
+++ tablerowvideo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+class TableRowVideo extends React.Component {
+constructor() {
+super();
+
+}
+
+render() {
+return (
+<tr id={this.props.video._id}>
+<td >
+<img alt={this.props.video.title} src={`${this.props.video.imgUrl}`} width='100' height='100'/>
+</td>
+<td>
+{this.props.video.title}
+</td> 
+
+
+<td>
+<h5>{this.props.video.description}</h5>
+</td> 
+
+<td>
+<a href={`${this.props.video.url}`} id= {`lnkImage_${this.props.video._id}`}>
+lien
+</a>
+</td> 
+
+<td>
+{this.props.video.publishedAt}
+</td> 
+</tr>
+
+
+);
+}
+}
+
+export default TableRowVideo;
