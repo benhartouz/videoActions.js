ee1b3101681a270a2f7526eed1bf6a7680e00516
hsibm <48910328+hsibm@users.noreply.github.com>
2019-04-12 17:22:24 +0200

    Update tablerowvideo.js

--- app/src/components/tablerowvideo.js
+++ app/src/components/tablerowvideo.js
@@ -7,31 +7,36 @@ super();
 }
 
 render() {
-return (
-<tr id={this.props.video._id}>
-<td >
-<img alt={this.props.video.title} src={`${this.props.video.imgUrl}`} width='100' height='100'/>
+return (<table border="2"  width="40%">
+    
+<tr id={this.props.video._id}  >
+<td colSpan="2" rowSpan="3">
+<img alt={this.props.video.title} src={`${this.props.video.imgUrl}`} width='95' height='95'/>
 </td>
-<td>
+<td colSpan="3" rowSpan="1">
 {this.props.video.title}
 </td> 
+</tr>
 
-
-<td>
+<tr>
+<td colSpan="3" rowSpan="1" >
 <h5>{this.props.video.description}</h5>
 </td> 
-
-<td>
+</tr>
+<tr>
+<td colSpan="1" rowSpan="1">
 <a href={`${this.props.video.url}`} >
 lien
 </a>
 </td> 
 
-<td>
+<td colSpan="2" rowSpan="1">
 {this.props.video.publishedAt}
 </td> 
 </tr>
 
+
+</table>
 
 );
 }
